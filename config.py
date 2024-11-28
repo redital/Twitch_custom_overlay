b93812dc8dc7030c6d71cb868937f82c531250ce
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2024-11-29 00:58:16 +0100

    Configurations fix

--- config.py
+++ config.py
@@ -2,10 +2,8 @@ import os
 
 flask_app_config = {
     "debug": os.environ.get("FLUSK_DEBUG_OPTION", True),
-    "host": os.environ.get("FLASK_HOST", None),
-    "port": os.environ.get("FLASK_PORT", 443),
-    "ssl_context": os.environ.get("SSL_CONTEX","adhoc"),
-    "allow_unsafe_werkzeug": os.environ.get("UNSAFE_WERKZEUG",True),
+    "host": os.environ.get("FLASK_HOST", "0.0.0.0"),
+    "port": os.environ.get("FLASK_PORT", 5000),
 }
 
 
